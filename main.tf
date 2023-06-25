3187e0e528fdecf581fc3c8eb4c8eb35e0dc310d
shegoj <shegoj@yahoo.com>
2023-06-25 20:37:36 +0100

    updated sales code to override machine name

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
-module "provision_ec2" {
+module "provision_ec2_1" {
   source = "git::https://github.com/techbleat/modules.git"
+  machine_name ="first machine"
 }
+
+module "provision_ec2_2" {
+  source = "git::https://github.com/techbleat/modules.git"
+  machine_name = "second machine"
+}
+
 
 terraform {
   required_providers {
